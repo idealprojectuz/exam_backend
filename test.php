8ca3989a40fd37b2db6459cb8667ce45b890a13f
Hayotbek <idealprojectuz@gmail.com>
2023-03-14 21:43:25 +0500

    f

--- test.php
+++ test.php
@@ -1,8 +1,7 @@
 <?php
-// echo '<pre>';
-// print_r($_SERVER);
-// echo '</pre>';
-echo base64_encode('@Hayotbek5096968');
-echo base64_decode(base64_encode('@Hayotbek5096968'));
+
+echo '<pre>';
+var_dump($_SERVER);
+echo '</pre>';
 
 ?>
